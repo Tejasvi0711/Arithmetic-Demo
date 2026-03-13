6f3159e1fce48848ef90b7d89f68644263a66d5b
Tejasvi0711 <tejasvi29git@gmail.com>
2026-03-13 15:07:51 +0530

    Add Arithmetic class with basic operations

---
+++ Arithmetic.java
@@ -0,0 +1,16 @@
+class Arithmetic
+{
+public static void main(String args[])
+{
+int a=100;
+int b=50;
+int sum=a+b;
+System.out.println("Sum of a and b"+sum);
+int sub=a-b;
+System.out.println("Substraction of a and b"+sub);
+int mul=a*b;
+System.out.println("Multiplication of a and b"+mul);
+int div=a/b;
+System.out.println("Division of a and b"+div);
+}
+}
